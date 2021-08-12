bae5bf0c2b5ffe2fd9ff4e3a27b115ae687f38e1
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2021-08-12 12:16:10 +0200

    API CoinMarketCap connection

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import os
 import requests
-import pprint
 
-ALPHA_VANTAGE_WEB = "https://www.alphavantage.co/query"
-ALPHA_API = os.environ.get("ALPHA_API")
+COINMARKET_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+COINMARKET_API = os.environ.get("COINMARKET_API")
 
 NEWSAPI_WEB = "https://newsapi.org/v2/everything"
 NEWS_API = os.environ.get("NEWS_API")
 
-alpha_params = {
-    "function": "TIME_SERIES_DAILY_ADJUSTED",
-    "symbol": "ETH",
-    "market": "USD",
-    "interval": "5min",
-    "apikey": ALPHA_API,
+parameters = {
+    "slug": "bitcoin,ethereum,ripple",
+    "convert": "EUR",
 }
 
-response = requests.get(ALPHA_VANTAGE_WEB, params=alpha_params)
+headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': COINMARKET_API,
+}
+
+response = requests.get(COINMARKET_URL, params=parameters, headers=headers)
 response.raise_for_status()
 
-pp = pprint.PrettyPrinter(indent=4)
-crypto_data = response.json()
-pp.pprint(crypto_data)
+crypto_data = response.json()['data']
+
+final_data = {}
+
+for key in crypto_data.keys():
+    value = crypto_data[key]["quote"]["EUR"]
+    coin = crypto_data[key]["name"]
+    final_data[coin] = value
+
+print(final_data)
