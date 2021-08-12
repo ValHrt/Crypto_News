91bf7c7d04e8122e1b4f8e90210a8f02ccfd523e
Valentin Henriot <59604857+ValHrt@users.noreply.github.com>
2021-08-12 19:51:16 +0200

    Connection with news API

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 import requests
+import datetime as dt
+import time
+# import pprint
 
 COINMARKET_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
 COINMARKET_API = os.environ.get("COINMARKET_API")
@@ -7,8 +10,12 @@ COINMARKET_API = os.environ.get("COINMARKET_API")
 NEWSAPI_WEB = "https://newsapi.org/v2/everything"
 NEWS_API = os.environ.get("NEWS_API")
 
+CRYPTO_NAME = ["bitcoin", "ethereum", "ripple"]
+
+today = dt.datetime.today().strftime("%Y-%m-%d")
+
 parameters = {
-    "slug": "bitcoin,ethereum,ripple",
+    "slug": ",".join(CRYPTO_NAME),
     "convert": "EUR",
 }
 
@@ -16,6 +23,37 @@ headers = {
     'Accepts': 'application/json',
     'X-CMC_PRO_API_KEY': COINMARKET_API,
 }
+
+news_data = {}
+
+for index, name in enumerate(CRYPTO_NAME):
+    newsapi_params = {
+        "qInTitle": CRYPTO_NAME[index],
+        "from": today,
+        "sortBy": "popularity",
+        "pageSize": 1,
+        "apiKey": NEWS_API,
+    }
+
+    response = requests.get(NEWSAPI_WEB, params=newsapi_params)
+    response.raise_for_status()
+
+    news_data[name] = response.json()["articles"][0]
+    time.sleep(1)
+
+# pp = pprint.PrettyPrinter(indent=4)
+# pp.pprint(news_data)
+
+news_text = []
+
+for key in news_data.keys():
+    news_text.append(f"Titre : {news_data[key]['title']}\nDescription : {news_data[key]['description']}"
+                     f"\nLien vers l'article : {news_data[key]['url']}\n{news_data[key]['urlToImage']}\nSource: "
+                     f"{news_data[key]['source']['name']}\n\n")
+
+# news_text = "".join(news_text)
+# print(news_text)
+
 
 response = requests.get(COINMARKET_URL, params=parameters, headers=headers)
 response.raise_for_status()
@@ -29,4 +67,34 @@ for key in crypto_data.keys():
     coin = crypto_data[key]["name"]
     final_data[coin] = value
 
-print(final_data)
+data_text = []
+emojis = ["🔺", "🔻"]
+
+
+def get_emoji(key: str, target: str):
+    if final_data[key][target] > 0:
+        emoji = emojis[0]
+    else:
+        emoji = emojis[1]
+    return emoji
+
+
+# print(final_data)
+
+for key in final_data.keys():
+    data_text.append(f"{key} :\nPrix actuel : {round(final_data[key]['price'], ndigits=2)} €\nVariation sur 24h : "
+                     f"{get_emoji(key, 'percent_change_24h')}{round(final_data[key]['percent_change_24h'], ndigits=2)}%"
+                     f"\nVariation sur une semaine : {get_emoji(key, 'percent_change_7d')}"
+                     f"{round(final_data[key]['percent_change_7d'], ndigits=2)}%\n")
+
+data_text[1], data_text[2] = data_text[2], data_text[1]
+# print(data_text)
+
+final_text = []
+
+for (data, news) in zip(data_text, news_text):
+    tmp = f"{data}{news}"
+    final_text.append(tmp)
+
+final_text = "".join(final_text)
+print(final_text)
